f592a157dd4549816d7e34caa6100e1972ce3e59
Ward Peeters <ward@coding-tech.com>
2018-11-09 21:26:00 +0100

    hacky work

--- examples/emotion/webpack.config.js
+++ examples/emotion/webpack.config.js
@@ -5,6 +5,7 @@ const VirtualModulePlugin = require('virtual-module-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  devtool: 'sourcemap',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
